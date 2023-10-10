8f830d467e0db332ddbcaf5337282deb35f4542c
Khush Mahajan <82937743+khush1998@users.noreply.github.com>
2023-10-10 12:10:15 +0530

    Add files via upload

---
+++ demo1docker/src/test/java/com/example/demo1docker/Demo1dockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo1docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1dockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
